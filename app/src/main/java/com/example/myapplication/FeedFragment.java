d6c602d90172edb913c8a084e1d640b035098ee8
Aditya99k <59930270+Aditya99k@users.noreply.github.com>
2020-10-17 03:07:16 +0530

    Comments Added

--- app/src/main/java/com/example/myapplication/FeedFragment.java
+++ app/src/main/java/com/example/myapplication/FeedFragment.java
@@ -25,58 +25,15 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.auth.User;
 
 public class FeedFragment extends Fragment {
-    private EditText SearchField;
-    private ImageView searchButton;
-    private RecyclerView resultList;
-    private FirebaseFirestore fstore;
-    private FirebaseAuth mAuth;
-   private String UserId;
 
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        inflater.inflate(R.menu.menu_search,menu);
-        MenuItem item=menu.findItem(R.id.action_search);
-        SearchView searchView=(SearchView) MenuItemCompat.getActionView(item);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                searchData(query);
-                return false;
-            }
 
-            private void searchData(String query) {
 
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-        super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()==R.id.action_search)
-        {
-            Toast.makeText(getContext(),"Search pressed",Toast.LENGTH_SHORT).show();
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View v=inflater.inflate(R.layout.fragment_feed,container,false);
-
-        SearchField=v.findViewById(R.id.searchBarEditText);
-        searchButton=v.findViewById(R.id.searchIcon);
-        resultList=v.findViewById(R.id.searchRecyclerView);
-        fstore=FirebaseFirestore.getInstance();
-        mAuth=FirebaseAuth.getInstance();
-        UserId=mAuth.getCurrentUser().getUid();
 
 
 
