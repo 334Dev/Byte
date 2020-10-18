6787f4e08eb7e0b7181be6e0234d0d3d7e70f243
Darpan Mittal <mittal11darpan@gmail.com>
2020-10-19 03:07:12 +0530

    Followers Following 1.0

--- app/src/main/java/com/example/myapplication/userSearch.java
+++ app/src/main/java/com/example/myapplication/userSearch.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -15,6 +16,7 @@ import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -32,16 +34,23 @@ public class userSearch extends AppCompatActivity implements userSearchAdapter.S
     private FirebaseFirestore fstore;
     private List<userSearchModel> userModel;
     private userSearchAdapter userAdapter;
+    private FirebaseAuth mAuth;
+    private String UserID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_search);
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
         UserSearchRecycler=findViewById(R.id.UserSearchRecycler);
         UserSearchText=findViewById(R.id.UserSearchText);
         UserSearchText.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_search_24, 0, 0, 0);
         fstore=FirebaseFirestore.getInstance();
+
+        mAuth = FirebaseAuth.getInstance();
+
+
 
         userModel=new ArrayList<>();
         userAdapter=new userSearchAdapter(userModel,this);
@@ -78,11 +87,17 @@ public class userSearch extends AppCompatActivity implements userSearchAdapter.S
             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
                 for (QueryDocumentSnapshot doc : value) {
 
+                    if(doc.getString("UserID").equals(mAuth.getCurrentUser().getUid())){
+                         Log.i("SameUser","Sameuser");
+                    }
+
+                    else{
                     Log.i("searchCheck", "onEvent:" + value.size());
 
                     userSearchModel set = doc.toObject(userSearchModel.class);
                     userModel.add(set);
                     userAdapter.notifyDataSetChanged();
+                }
                 }
             }
         });
@@ -93,11 +108,16 @@ public class userSearch extends AppCompatActivity implements userSearchAdapter.S
     @Override
     public void selectedItem(userSearchModel userModel) {
 
-        Intent i=new Intent(userSearch.this,AnotherUserProfile.class);
-        i.putExtra("SearchUserID",userModel.UserID);
-        Log.i("sentIntent", "selectedItem: "+userModel.UserID);
 
-        startActivity(i);
+
+              Intent i = new Intent(userSearch.this, AnotherUserProfile.class);
+              i.putExtra("SearchUserID", userModel.UserID);
+              Log.i("sentIntent", "selectedItem: " + userModel.UserID);
+              Log.i("currentUser", "CurrentUserid: " + mAuth.getCurrentUser().getUid());
+
+              startActivity(i);
+          }
+
+
 
     }
-}
