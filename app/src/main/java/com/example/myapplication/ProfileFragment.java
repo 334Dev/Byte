3b58afb33a198dc109130eb699013e620e68d844
Darpan Mittal <mittal11darpan@gmail.com>
2020-10-21 01:07:43 +0530

    Profile Fragmant posts

--- app/src/main/java/com/example/myapplication/ProfileFragment.java
+++ app/src/main/java/com/example/myapplication/ProfileFragment.java
@@ -7,6 +7,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,23 +20,34 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import jp.wasabeef.picasso.transformations.BlurTransformation;
 
 import static android.app.Activity.RESULT_OK;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements LatestAdapter.SelectedItem{
 
     private CircleImageView profileImageView;
     private TextView userName;
@@ -48,6 +60,11 @@ public class ProfileFragment extends Fragment {
     private TextView post,followers,following;
     private AlertDialog.Builder builder;
     private AlertDialog show;
+    private LatestAdapter ProfilePostsAdapter;
+    private List<Model_Latest> ProfilePostsItem;
+    private RecyclerView profileRecyclerView;
+    private DocumentSnapshot lastProfilePost;
+    private Query query;
 
     @Nullable
     @Override
@@ -79,6 +96,36 @@ public class ProfileFragment extends Fragment {
         storageReference = FirebaseStorage.getInstance().getReference();
 
 
+
+        profileRecyclerView=root.findViewById(R.id.ProfileRecyclerView);
+        profileRecyclerView.setHasFixedSize(true);
+        profileRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        ProfilePostsItem=new ArrayList<>();
+        ProfilePostsAdapter=new LatestAdapter(ProfilePostsItem,this);
+        profileRecyclerView.setAdapter(ProfilePostsAdapter);
+        profileRecyclerView.setHasFixedSize(true);
+
+        profileRecyclerView.setNestedScrollingEnabled(false);
+
+        profileRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                LinearLayoutManager linearLayoutManager= (LinearLayoutManager) profileRecyclerView.getLayoutManager();
+                if(linearLayoutManager.findLastCompletelyVisibleItemPosition()==profileRecyclerView.getChildCount()){
+                    setProfilePosts();
+                }
+            }
+        });
+
+
+          setProfilePosts();
 
 
         //getImage
@@ -119,6 +166,51 @@ public class ProfileFragment extends Fragment {
 
 
     }
+
+    private void setProfilePosts() {
+
+
+         if(lastProfilePost==null)
+         {
+             query=fstore.collection("Post")
+                     .orderBy("time", Query.Direction.DESCENDING)
+                     .whereEqualTo("Owner",UserID)
+                     .limit(1000);
+
+         }else{
+             query=fstore.collection("Post")
+                     .orderBy("time", Query.Direction.DESCENDING)
+                     .whereEqualTo("Owner",UserID)
+                     .startAfter(lastProfilePost)
+                     .limit(1000);
+         }
+
+         query.addSnapshotListener(new EventListener<QuerySnapshot>() {
+             @Override
+             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+               if(value.isEmpty())
+                   Toast.makeText(getContext(),"You have no posts",Toast.LENGTH_SHORT).show();
+               else {
+
+                   for (QueryDocumentSnapshot doc : value) {
+
+                       Log.i("dataRecieveCHeck", "onEvent:" + value.size());
+                       Model_Latest set = doc.toObject(Model_Latest.class);
+                       ProfilePostsItem.add(set);
+                       ProfilePostsAdapter.notifyDataSetChanged();
+                       show.dismiss();
+
+                   }
+                   lastProfilePost = value.getDocuments().get(value.size() - 1);
+               }
+
+             }
+         });
+
+
+
+    }
+
     //setting user profile details; post, followers, following
     private void setUserDetails() {
         fstore.collection("Users").document(UserID).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -136,6 +228,8 @@ public class ProfileFragment extends Fragment {
                 show.dismiss();
             }
         });
+
+
     }
 
     @Override
@@ -173,4 +267,12 @@ public class ProfileFragment extends Fragment {
 
     }
 
+    @Override
+    public void selectedItem(Model_Latest model_latest) {
+
+        Intent i=new Intent(getActivity(),ViewPost.class);
+        i.putExtra("PostId",model_latest.ID);
+        startActivity(i);
+
+    }
 }
