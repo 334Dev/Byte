83e9d4abb9d61f39ddfb6582da520142d3663590
Aditya99k <kashyapaditya1234@gmail.com>
2020-10-29 01:43:39 +0530

    ShowQuick loading modified

--- app/src/main/java/com/example/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.widget.NestedScrollView;
@@ -19,7 +18,6 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
-
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,7 +28,6 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
--- app/src/main/java/com/example/myapplication/QuickFragment.java
+++ app/src/main/java/com/example/myapplication/QuickFragment.java
@@ -13,6 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -39,6 +40,7 @@ public class QuickFragment extends Fragment implements quickAdapter.SelectedPage
     private Query query;
     private Integer index;
     private ProgressBar loading;
+    private View parentLayout;
 
     @Nullable
     @Override
@@ -50,6 +52,7 @@ public class QuickFragment extends Fragment implements quickAdapter.SelectedPage
 
         viewPager=v.findViewById(R.id.quickPager);
         loading=v.findViewById(R.id.quickLoading);
+
 
         loading.setVisibility(View.VISIBLE);
         
@@ -107,7 +110,8 @@ public class QuickFragment extends Fragment implements quickAdapter.SelectedPage
                 @Override
                 public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
                     if(value.isEmpty()){
-                        Log.i("PostEmpty", "onEvent: Empty");
+                        Toast.makeText(getContext(),"No posts to show",Toast.LENGTH_SHORT).show();
+
                     }else {
                         for (QueryDocumentSnapshot doc : value) {
 
@@ -120,12 +124,14 @@ public class QuickFragment extends Fragment implements quickAdapter.SelectedPage
                         }
                        // index=index+value.size()-1;
                         //Log.i("PostIndex", "onEvent: "+index);
-                        loading.setVisibility(View.INVISIBLE);
+                        //loading.setVisibility(View.INVISIBLE);
 
                         lastQuick = value.getDocuments().get(value.size() - 1);
                         Log.i("PostLast", "onEvent: "+quickModels.size());
                         Log.i("PostLast", "onEvent: "+lastQuick.getId());
                     }
+
+                    loading.setVisibility(View.INVISIBLE);
                 }
             });
 
